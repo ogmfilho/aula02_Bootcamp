cbfb7f66031b7387e15f7fcc853104484d9202a8
Otoniel Miranda <67445518+ogmfilho@users.noreply.github.com>
2024-08-07 17:58:35 -0300

    Depois de fazer alguns testes

---
+++ exercicio.py
@@ -0,0 +1,14 @@
+# 2) Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+# 4) Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+# 5) Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+
+# 7) Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário
+# 13) Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+# 14) Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+
+# 19)Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+# 20)Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
+
+teste = True
+
+print(not teste)
